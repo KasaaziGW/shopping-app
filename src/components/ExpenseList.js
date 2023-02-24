d0d599e6b7fe9919a415f4b8b5dea4f98fd76f3c
KasaaziGW <kasaazi.gw@gmail.com>
2023-02-24 04:48:32 +0200

    Changes to CartValue and ExpenseList

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,7 +1,28 @@
 import React, { useContext } from 'react';
+import ExpenseItem from './ExpenseItem'
 import { AppContext } from '../context/AppContext';
 
 const ExpenseList = () => {
+    const { expenses } = useContext(AppContext);
+
+    return (
+        <table className='table'>
+            <thead className='thead-light'>
+                <tr>
+                    <td scope='col'>Items</td>
+                    <td scope='col'>Quantity</td>
+                    <td scope='col'>Unit Price</td>
+                    <td scope='col'>Item Price</td>
+                    <td scope='col'>Remove</td>
+                </tr>
+            </thead>
+            <tbody>
+                {expenses.map((expense) => (
+                    <ExpenseItem id={expense.id} key={expense.id} name={expense.name} quantity={expense.quantity} unitprice={expense.unitprice}/>
+                ))};
+            </tbody>
+        </table>
+    );
 };
 
 export default ExpenseList;
